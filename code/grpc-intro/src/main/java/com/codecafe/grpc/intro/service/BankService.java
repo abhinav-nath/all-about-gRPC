6bd0b5acb0d09c7a2cf211fdae3004ae9f26b697
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-08-01 00:52:30 +0530

    added example for server streaming rpc

--- code/grpc-intro/src/main/java/com/codecafe/grpc/intro/service/BankService.java
+++ code/grpc-intro/src/main/java/com/codecafe/grpc/intro/service/BankService.java
@@ -2,6 +2,8 @@ package com.codecafe.grpc.intro.service;
 
 import com.codecafe.grpc.intro.model.Balance;
 import com.codecafe.grpc.intro.model.BalanceCheckRequest;
+import com.codecafe.grpc.intro.model.WithdrawRequest;
+import com.codecafe.grpc.intro.model.WithdrawResponse;
 import com.codecafe.grpc.intro.persistence.AccountDatabase;
 import io.grpc.stub.StreamObserver;
 
@@ -17,6 +19,32 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                 .build();
 
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withdraw(WithdrawRequest request, StreamObserver<WithdrawResponse> responseObserver) {
+        int accountNumber = request.getAccountNumber();
+        int amountToWithdraw = request.getAmount();
+
+        int balance = AccountDatabase.getBalance(accountNumber);
+
+        for (int i = 0; i < (amountToWithdraw / 10); i++) {
+            WithdrawResponse response = WithdrawResponse.newBuilder()
+                    .setAmount(10)
+                    .build();
+            responseObserver.onNext(response);
+
+            AccountDatabase.deductBalance(accountNumber, 10);
+
+            // simulate a delay of 1s for every $10
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         responseObserver.onCompleted();
     }
 
